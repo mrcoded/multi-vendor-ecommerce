bbcf165280e290ebaee63a303b9335ed586dfb99
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-19 15:46:20 +0100

    feat: initialized project file and structure, and installed scss

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h2 className="text-4xl">Welcome to Multi vendor E-commerce</h2>
+    </div>
+  );
+}
