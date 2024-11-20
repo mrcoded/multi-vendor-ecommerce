dc3499d026772861d602bf30a0d684a6159f4df2
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-20 02:29:00 +0100

    feat: built a weeklysaleschart component using piechart from react-chartjs-2 to show chart of top products sales

---
+++ src/app/dashboard/_components/charts/BestSellingProductsChart.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+function BestSellingProductsChart() {
+  const data = {
+    labels: ["Poultry", "Vegetables", "Fruits", "Meat"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [50, 10, 20, 20],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.7)",
+          "rgba(54, 162, 235, 0.7)",
+          "rgba(255, 206, 86, 0.7)",
+          "rgba(75, 192, 192, 0.7)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  return (
+    <div className="bg-slate-700 p-8 rounded-lg">
+      <h2 className="text-xl font-bold mb-4">Best Selling Products</h2>
+      {/* Best Selling Products Pie Chart */}
+      <div className="p-4">
+        <Pie data={data} className="" />
+      </div>
+    </div>
+  );
+}
+
+export default BestSellingProductsChart;
