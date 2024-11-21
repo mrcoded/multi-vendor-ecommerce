ca224bdb47a7b79ec59d0d868f3faa479e2fdf8e
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-21 23:13:33 +0100

    chore: adjusted styles

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,11 +12,11 @@ export default function dashboardLayout({
     <div className="flex">
       {/* Sidebar */}
       <Sidebar />
-      <div className="w-full">
-        {/* Navbar */}
+      <div className="lg:ml-64 ml-0 flex-grow bg-slate-100 min-h-screen">
+        {/* Header */}
         <Navbar />
         {/* Main */}
-        <main className="ml-60 p-8 bg-slate-900 text-slate-50 min-h-screen mt-16">
+        <main className="p-8 bg-slate-100 dark:bg-slate-900 text-slate-50  mt-16">
           {children}
         </main>
       </div>
