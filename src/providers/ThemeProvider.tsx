e08e1827216589a2bb9383f48da06acbfc03bb58
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-24 14:46:07 +0100

    feat: added react-hot-toast

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -3,9 +3,15 @@
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+import { Toaster } from "react-hot-toast";
+
 export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <Toaster position="top-center" reverseOrder={false} /> {children}
+    </NextThemesProvider>
+  );
 }
