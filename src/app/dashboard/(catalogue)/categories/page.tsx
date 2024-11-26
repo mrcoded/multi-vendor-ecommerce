be5ea81664da2aee8a521993a356a3324e2b7a19
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-26 18:32:41 +0100

    feat: created a categories page

---
+++ src/app/dashboard/(catalogue)/categories/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import PageHeader from "../_components/PageHeader";
+import TableActions from "../_components/TableActions";
+
+const page = () => {
+  return (
+    <div>
+      {/* Header */}
+      <PageHeader
+        heading="Categoties"
+        href="/dashboard/categories/new"
+        linkAction="Add Category"
+      />
+      {/* Table Actions */}
+      <TableActions />
+
+      <div className="py-8">
+        <h2>Table</h2>
+      </div>
+    </div>
+  );
+};
+
+export default page;
