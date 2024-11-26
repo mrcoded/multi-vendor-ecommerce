5410bee5c40020ff582be5b58fbc9fc8ff99ae9a
CodedLibra <adeola.ogundijo@yahoo.com>
2024-11-26 18:49:26 +0100

    feat: created a re-usable TextInput component

---
+++ src/components/inputs/TextInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { FieldValues, UseFormRegister, FieldErrors } from "react-hook-form";
+
+interface TextInputProps {
+  label: string;
+  name: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  isRequired?: boolean;
+  type?: string;
+  className?: string;
+  defaultValue?: string;
+}
+function TextInput({
+  label,
+  name,
+  register,
+  errors,
+  isRequired = true,
+  type = "text",
+  className = "sm:col-span-2",
+  defaultValue = "",
+}: TextInputProps) {
+  return (
+    <div className={className}>
+      <label
+        htmlFor={name}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-slate-50 leading-6"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          {...register(`${name}`, { required: isRequired })}
+          type={type}
+          name={name}
+          id={name}
+          defaultValue={defaultValue}
+          autoComplete={name}
+          className="block w-full rounded-md border-0 py-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lime-700 dark:focus:ring-slate-500 sm:text-sm sm:leading-6 dark:bg-transparent dark:text-slate-100"
+          placeholder={`Type the ${label.toLowerCase()}`}
+        />
+        {errors[`${name}`] && (
+          <span className="text-sm text-red-600">{label} is required</span>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TextInput;
