0d112341eab3344bfc978165f98de8fe89e89080
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-03 16:58:21 +0100

    fix: added register from RHF for input value

--- src/components/inputs/ToggleInput.tsx
+++ src/components/inputs/ToggleInput.tsx
@@ -15,6 +15,7 @@ function ToggleInput({
   name,
   truthyValue,
   falsyValue,
+  register,
   className = "sm:col-span-2 flex flex-wrap",
 }: ToggleInputProps) {
   const [value, setValue] = useState(true);
@@ -29,11 +30,11 @@ function ToggleInput({
       <div className="w-full sm:w-1/2">
         <label className="relative inline-flex items-center cursor-pointer">
           <input
-            value={name}
+            {...register(name)}
             type="checkbox"
             checked={value}
-            onChange={(e) => setValue(e.target.checked ? true : false)}
             className="sr-only peer"
+            onChange={(e) => setValue(e.target.checked)}
           />
           <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-purple-300 dark:peer-focus:ring-purple-800 rounded-full peer-focus:dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-purple-600"></div>
           <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
