41fff782118cef04b16eb6557c49e3bdcd3a6a23
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-04 23:27:25 +0100

    feat: created a GET request for Coupons api route

--- src/app/api/coupons/route.ts
+++ src/app/api/coupons/route.ts
@@ -3,13 +3,14 @@ import { db } from "@/lib/db";
 
 export async function POST(request: Request) {
   try {
-    const { title, couponCode, expiryDate } = await request.json();
+    const { title, couponCode, expiryDate, isActive } = await request.json();
 
     const newCoupon = await db.coupon.create({
       data: {
         title,
         couponCode,
         expiryDate,
+        isActive,
       },
     });
     console.log(newCoupon);
@@ -21,6 +22,30 @@ export async function POST(request: Request) {
     return NextResponse.json(
       {
         message: "Unable to create Coupon",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const coupons = await db.coupon.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(coupons);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch Coupons",
         error,
       },
       {
