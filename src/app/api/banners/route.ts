b2b3669dd79f6ca4a65f74cbf84ea6348c46967b
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-04 23:13:02 +0100

    feat: created a POST request for banners api route

---
+++ src/app/api/banners/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST(request: Request) {
+  try {
+    const { title, link, imageUrl, isActive } = await request.json();
+
+    const newBanner = await db.banner.create({
+      data: {
+        title,
+        link,
+        imageUrl,
+        isActive,
+      },
+    });
+    console.log(newBanner);
+
+    return NextResponse.json(newBanner);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to create Banner",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
