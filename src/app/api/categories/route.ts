3e7e69b6de6e72f933a8b8735fbff4a9e8d9b4b1
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-06 17:03:27 +0100

    feat: created a GET request for categories api route and checked for existingCategory in the POST request

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -6,6 +6,21 @@ export async function POST(request: Request) {
     const { title, slug, imageUrl, description, isActive } =
       await request.json();
 
+    const existingCategory = await db.category.findUnique({
+      where: {
+        slug,
+      },
+    });
+
+    if (existingCategory) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "Category already exists",
+        },
+        { status: 409 }
+      );
+    }
     const newCategory = await db.category.create({
       data: { title, slug, imageUrl, description, isActive },
     });
@@ -16,6 +31,30 @@ export async function POST(request: Request) {
     return NextResponse.json(
       {
         message: "Unable to create Category",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function GET(request: Request) {
+  try {
+    const categories = await db.category.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(categories);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch Category",
         error,
       },
       {
