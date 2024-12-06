1e9a45da0a829ea58e304b366f326fc76d7c8a58
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-06 16:47:42 +0100

    feat: created a POST request for users api route

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import bcrypt from "bcrypt";
+
+export async function POST(request: Request) {
+  try {
+    const { name, password, email } = await request.json();
+
+    //check if user already exists in the db
+    const existingUser = await db.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "User Already exists",
+        },
+        { status: 409 }
+      );
+    }
+
+    //Encrypt password with bcrypt
+    const hashedPassword = await bcrypt.hash(password, 16);
+
+    const newUser = await db.user.create({
+      data: {
+        name,
+        password: hashedPassword,
+        email,
+      },
+    });
+
+    console.log(newUser);
+
+    return NextResponse.json(
+      { data: newUser, message: "User Created Successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to create new User",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
