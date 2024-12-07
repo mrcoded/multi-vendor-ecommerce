f81e0872450ca640eb5111cb870d27b2633cd785
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-07 18:57:49 +0100

    feat: created an onboarding id page

---
+++ src/app/onboarding/[id]/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import getUserData from "@/lib/getUserData";
+import NewVendorForm from "@/components/forms/NewVendorForm";
+
+const page = async ({ params: { id } }: { params: { id: string } }) => {
+  //GET user by ID
+  const user = await getUserData(`users/${id}`);
+
+  return (
+    <div className="flex flex-col gap-6 p-16">
+      <div className="max-w-4xl p-4 mx-auto">
+        <h2>Hello {user?.name} Tell Us More About Yourself</h2>
+      </div>
+      <NewVendorForm user={user} />
+    </div>
+  );
+};
+
+export default page;
