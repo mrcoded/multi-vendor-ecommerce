47672ef6e65d403e953fae259f4cf97716976cdd
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-07 19:04:37 +0100

    feat: created  a GET request for the User api route

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,33 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const users = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    console.log(users);
+    return NextResponse.json(users);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch User",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
