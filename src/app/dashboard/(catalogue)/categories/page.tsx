19477a3be9f9ce666be27589e7db98b80c4dccc0
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-07 18:47:42 +0100

    chore

--- src/app/dashboard/(catalogue)/categories/page.tsx
+++ src/app/dashboard/(catalogue)/categories/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
-import PageHeader from "../_components/PageHeader";
-import TableActions from "../_components/TableActions";
+import PageHeader from "../../_components/PageHeader";
+import TableActions from "../../_components/TableActions";
 
 const page = () => {
   return (
     <div>
       {/* Header */}
       <PageHeader
-        heading="Categoties"
+        heading="Categories"
         href="/dashboard/categories/new"
         linkAction="Add Category"
       />
+
       {/* Table Actions */}
       <TableActions />
 
--- src/components/buttons/SubmitButton.tsx
+++ src/components/buttons/SubmitButton.tsx
@@ -27,7 +27,7 @@ function SubmitButton({
           className="inline-flex items-center px-5 py-3 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-slate-900 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-slate-800 dark:bg-lime-600 dark:hover:bg-lime-700"
         >
           <Plus className="w-5 h-5 mr-2" />
-          <span>Save {buttonTitle}</span>
+          <span>{buttonTitle}</span>
         </button>
       )}
     </div>
