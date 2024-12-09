a74f3a62e00f5bd6eb10de2c6597ee6c1e92d495
CodedLibra <adeola.ogundijo@yahoo.com>
2024-12-09 17:04:13 +0100

    feat: created a getUserData function

---
+++ src/lib/getUserData.ts
@@ -0,0 +1,16 @@
+async function getUserData(endpoint: string) {
+  try {
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+    const response = await fetch(`${baseUrl}/api/${endpoint}`, {
+      cache: "no-store",
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default getData;
