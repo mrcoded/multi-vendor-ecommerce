49704013229c84f24c68126a5da1ca56c9c98fdb
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-01 23:53:08 +0100

    feat: created datacolumn component for a re-usable datatable date display

---
+++ src/components/tables/DataTable/DataColumn.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row } from "@tanstack/react-table";
+import { RowDatas } from "@/app/dashboard/(catalogue)/categories/columns";
+
+// This type is used to define the shape of our data.
+function DataColumn({
+  row,
+  accessorKey,
+}: {
+  row: Row<RowDatas>;
+  accessorKey: string;
+}) {
+  const createdAt = row.getValue(`${accessorKey}`) as string;
+  const originalDate = new Date(createdAt);
+
+  const day = originalDate.getDate();
+  const month = originalDate.toLocaleString("default", { month: "short" });
+  const year = originalDate.getFullYear();
+
+  return <div className="line-clamp-1">{`${day} ${month}, ${year}`}</div>;
+}
+
+export default DataColumn;
