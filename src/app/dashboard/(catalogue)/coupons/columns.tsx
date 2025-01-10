548f35906b7f26f53bfb3164e7211704b3c83dc9
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-10 23:46:21 +0100

    feat: added a column component for dashboard/Coupons

---
+++ src/app/dashboard/(catalogue)/coupons/columns.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { RowDatas } from "../categories/columns";
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
+import DataColumn from "@/components/tables/DataTable/DataColumn";
+import ImageColumn from "@/components/tables/DataTable/ImageColumn";
+import SortableColumn from "@/components/tables/DataTable/SortableColumn";
+import ActiveStatusColumn from "@/components/tables/DataTable/ActiveStatusColumn";
+
+export const columns: ColumnDef<RowDatas>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "title",
+    header: ({ column }) => <SortableColumn column={column} title="Title" />,
+  },
+  {
+    accessorKey: "couponCode",
+    header: "Coupon Code",
+    cell: ({ row }) => {
+      const couponCode = row.getValue("couponCode") as string;
+
+      return <div className="line-clamp-1">{couponCode}</div>;
+    },
+  },
+  {
+    accessorKey: "expiryDate",
+    header: "Expiry Date",
+    cell: ({ row }) => <DataColumn row={row} accessorKey="expiryDate" />,
+  },
+  {
+    accessorKey: "isActive",
+    header: "Active",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date Created",
+    cell: ({ row }) => <DataColumn row={row} accessorKey="createdAt" />,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const coupon = row.original;
+
+      return (
+        <ActiveStatusColumn
+          row={row}
+          title="Coupon"
+          endpoint={`coupons/${coupon.id}`}
+          editEndpoint={`coupons/update/${coupon.id}`}
+        />
+      );
+    },
+  },
+];
