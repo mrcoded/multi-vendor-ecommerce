07af499cbc9a201428dc6afd95c21c43bcd210d6
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-11 14:57:12 +0100

    feat: built an updateBanner page

---
+++ src/app/dashboard/(catalogue)/banners/(banners-actions)/update/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import BannerForm from "@/components/forms/BannerForm";
+import FormHeader from "@/app/dashboard/_components/FormHeader";
+
+const UpdateBanner = async ({ params: { id } }: { params: { id: string } }) => {
+  const banner = await getData(`banners/${id}`);
+
+  return (
+    <div>
+      <FormHeader title="Update Banner" />
+      <BannerForm updateData={banner} />
+    </div>
+  );
+};
+
+export default UpdateBanner;
