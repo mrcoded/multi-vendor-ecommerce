a1b3c055845089048d8bb08f74e4c5b4b6ecb71f
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-11 14:57:53 +0100

    feat: created a banners page

---
+++ src/app/dashboard/(catalogue)/banners/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import { columns } from "./columns";
+import { DataTable } from "@/components/tables/DataTable/page";
+
+import PageHeader from "../../_components/PageHeader";
+import TableActions from "../../_components/TableActions";
+
+const page = async () => {
+  const banners = await getData("banners");
+
+  return (
+    <div>
+      {/* Header */}
+      <PageHeader
+        heading="Banners"
+        href="/dashboard/banners/new"
+        linkAction="Add Banner"
+      />
+
+      {/* Table Actions */}
+      <TableActions />
+
+      <div className="py-8">
+        <DataTable data={banners} columns={columns} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
