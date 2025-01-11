ac083d7ed3e3c3473b029d706374502d76a2e4cc
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-11 14:52:44 +0100

    chore: extracted BannerForm component

--- src/app/dashboard/(catalogue)/banners/(banners-actions)/new/page.tsx
+++ src/app/dashboard/(catalogue)/banners/(banners-actions)/new/page.tsx
@@ -1,99 +1,13 @@
-"use client";
+import React from "react";
 
-import React, { useState } from "react";
-import { useRouter } from "next/navigation";
-
-import { FieldValues, useForm } from "react-hook-form";
-
-import { generateSlug } from "@/lib/generateSlug";
-import { makePostRequest } from "@/lib/apiRequest";
-
-import TextInput from "@/components/inputs/TextInput";
-import ImageInput from "@/components/inputs/ImageInput";
-import ToggleInput from "@/components/inputs/ToggleInput";
-import SubmitButton from "@/components/buttons/SubmitButton";
+import BannerForm from "@/components/forms/BannerForm";
 import FormHeader from "@/app/dashboard/_components/FormHeader";
 
 const NewBanner = () => {
-  const router = useRouter();
-  const [imageUrl, setImageUrl] = useState("");
-  const [loading, setLoading] = useState(false);
-
-  const {
-    register,
-    reset,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const redirectUrl = () => {
-    router.push("/dashboard/banners");
-  };
-
-  const onSubmit = async (data: FieldValues) => {
-    if (!data.title || !data.link) return;
-
-    const slug = generateSlug(data.title);
-    data.slug = slug;
-    data.imageUrl = imageUrl;
-
-    makePostRequest({
-      setLoading,
-      endpoint: "api/banners",
-      data,
-      resourceName: "Banner",
-      reset,
-      redirectUrl,
-    });
-
-    setImageUrl("");
-  };
-
   return (
     <div>
       <FormHeader title="New Banner" />
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3"
-      >
-        <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-          <TextInput
-            label="Banner Title"
-            name="title"
-            register={register}
-            errors={errors}
-          />
-
-          <TextInput
-            label="Banner Link"
-            name="link"
-            type="url"
-            register={register}
-            errors={errors}
-          />
-
-          <ImageInput
-            imageUrl={imageUrl}
-            setImageUrl={setImageUrl}
-            endpoint="bannerImageUploader"
-            label="Banner Image"
-          />
-
-          <ToggleInput
-            label="Publish your Banner"
-            name="isActive"
-            truthyValue="Active"
-            falsyValue="Draft"
-            register={register}
-          />
-        </div>
-
-        <SubmitButton
-          isLoading={loading}
-          buttonTitle="Create Banner"
-          loadingButtonTitle="Creating Banner please wait..."
-        />
-      </form>
+      <BannerForm />
     </div>
   );
 };
