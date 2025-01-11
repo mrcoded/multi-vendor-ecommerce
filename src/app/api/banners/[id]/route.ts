bfc4ef67b495c98fdd60b4600d27b66dd6eebf86
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-11 14:39:49 +0100

    feat: added PUT, GET, and DELETE request to banners/[id] api route

---
+++ src/app/api/banners/[id]/route.ts
@@ -0,0 +1,132 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const banner = await db.banner.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json(banner);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch Banner",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const existingBanner = await db.banner.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingBanner) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "Banner Not Found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const deletedBanner = await db.banner.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json(deletedBanner);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to Delete Banner",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function PUT(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const { title, link, imageUrl, isActive } = await request.json();
+
+    const existingBanner = await db.banner.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingBanner) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "Banner not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    const updatedBanner = await db.banner.update({
+      where: {
+        id,
+      },
+      data: { title, link, imageUrl, isActive },
+    });
+
+    return NextResponse.json(updatedBanner);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to update Banner",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
