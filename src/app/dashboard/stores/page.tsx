636962caaa0c60bb984229b856004359fbf34569
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-12 16:24:10 +0100

    feat: built a stores page

---
+++ src/app/dashboard/stores/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import { columns } from "./columns";
+import { DataTable } from "@/components/tables/DataTable/page";
+
+import PageHeader from "../_components/PageHeader";
+import TableActions from "../_components/TableActions";
+
+const page = async () => {
+  const stores = await getData("stores");
+
+  return (
+    <div>
+      {/* Header */}
+      <PageHeader
+        heading="Stores"
+        href="/dashboard/stores/new"
+        linkAction="Add Store"
+      />
+
+      {/* Table Actions */}
+      <TableActions />
+
+      <div className="py-8">
+        <DataTable data={stores} columns={columns} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
