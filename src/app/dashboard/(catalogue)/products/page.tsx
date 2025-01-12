8b81d12f9a9bb49578ad08e995625ebc9d681caa
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-12 16:27:30 +0100

    feat: built a dashboard/products page

---
+++ src/app/dashboard/(catalogue)/products/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import { columns } from "./columns";
+import { DataTable } from "@/components/tables/DataTable/page";
+
+import PageHeader from "../../_components/PageHeader";
+import TableActions from "../../_components/TableActions";
+
+const page = async () => {
+  const products = await getData("products");
+
+  return (
+    <div>
+      {/* Header */}
+      <PageHeader
+        heading="Products"
+        href="/dashboard/products/new"
+        linkAction="Add Product"
+      />
+
+      {/* Table Actions */}
+      <TableActions />
+
+      <div className="py-8">
+        <DataTable data={products} columns={columns} />
+      </div>
+    </div>
+  );
+};
+
+export default page;
