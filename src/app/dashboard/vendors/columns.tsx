259c232ebefa779a4b7b2617072aac61adc26200
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-12 16:48:21 +0100

    feat: added columns and datatable components for vendors page

---
+++ src/app/dashboard/vendors/columns.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { RowDatas } from "../(catalogue)/categories/columns";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
+import DataColumn from "@/components/tables/DataTable/DataColumn";
+import ImageColumn from "@/components/tables/DataTable/ImageColumn";
+import SortableColumn from "@/components/tables/DataTable/SortableColumn";
+import ActiveStatusColumn from "@/components/tables/DataTable/ActiveStatusColumn";
+
+export const columns: ColumnDef<RowDatas>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => <SortableColumn column={column} title="Name" />,
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+    cell: ({ row }) => <DataColumn row={row} accessorKey="Email" />,
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+    cell: ({ row }) => <DataColumn row={row} accessorKey="Role" />,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date Created",
+    cell: ({ row }) => <DataColumn row={row} accessorKey="createdAt" />,
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const vendor = row.original;
+
+      return (
+        <ActiveStatusColumn
+          row={row}
+          title="Vendor"
+          endpoint={`vendors/${vendor.id}`}
+          editEndpoint={`vendors/update/${vendor.id}`}
+        />
+      );
+    },
+  },
+];
