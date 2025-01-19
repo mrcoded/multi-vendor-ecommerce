71a059dea34cbf3fc376ebdf6cf957b515405292
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-19 19:45:22 +0100

    feat: added session from next auth to render UserAvatar component if user is authenticated and a CartCounter component from the cart page actions

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import React from "react";
-import { ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
+import { User } from "lucide-react";
 
+import { useSession } from "next-auth/react";
+
+import UserAvatar from "../UserAvatar";
 import SearchForm from "../forms/SearchForm";
 import { HelpModal } from "../modals/HelpModal";
 import ThemeSwitcherButton from "../ThemeSwitcherButton";
+import CartCounter from "@/app/cart/_components/CartCounter";
 
 function Navbar() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className="bg-whihte dark:bg-slate-700">
+    <div className="bg-white dark:bg-slate-700">
       <div className="flex items-center justify-between py-3 max-w-7xl mx-auto px-8 gap-8">
         {/* Logo */}
         <Link className="" href="/">
@@ -22,27 +34,20 @@ function Navbar() {
         </div>
 
         <div className="flex gap-8">
-          <Link
-            href="/"
-            className="flex items-center space-x-1 text-green-950 dark:text-slate-100"
-          >
-            <User />
-            <span>Login</span>
-          </Link>
+          {status === "unauthenticated" ? (
+            <Link
+              href="/login"
+              className="flex items-center space-x-1 text-green-950 dark:text-slate-100"
+            >
+              <User />
+              <span>Login</span>
+            </Link>
+          ) : (
+            session?.user && <UserAvatar user={session.user} />
+          )}
 
           <HelpModal />
-
-          <Link
-            href="/cart"
-            type="button"
-            className="relative inline-flex items-center p-3 text-sm font-medium text-center text-white bg-transparent rounded-lg"
-          >
-            <ShoppingCart className="text-lime-700 dark:text-lime-500" />
-            <span className="sr-only">Cart</span>
-            <div className="absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white bg-red-500 rounded-full -top-0 end-6 dark:border-gray-900">
-              20
-            </div>
-          </Link>
+          <CartCounter />
         </div>
         <ThemeSwitcherButton />
       </div>
