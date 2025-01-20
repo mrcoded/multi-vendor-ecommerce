8d50a91bb84ceb8aec0cc116fe9b12b064183f56
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-20 19:07:22 +0100

    feat: created a stepform component

---
+++ src/app/checkout/_components/StepForm.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+
+import { RootState } from "@/redux/types";
+import { useSelector } from "react-redux";
+
+import OrderSummary from "./OrderSummary";
+import PaymentMethodForm from "@/components/forms/StepForm/PaymentMethodForm";
+import PersonalDetailsForm from "@/components/forms/StepForm/PersonalDetailsForm";
+import ShippingDetailsForm from "@/components/forms/StepForm/ShippingDetailsForm";
+
+const StepForm = () => {
+  const currentStep = useSelector(
+    (state: RootState) => state.checkout.currentStep
+  );
+  const renderFormByStep = (step: number) => {
+    if (step === 1) return <PersonalDetailsForm />;
+    if (step === 2) return <ShippingDetailsForm />;
+    if (step === 3) return <PaymentMethodForm />;
+    if (step === 4) return <OrderSummary />;
+  };
+  return <div>{renderFormByStep(currentStep)}</div>;
+};
+
+export default StepForm;
