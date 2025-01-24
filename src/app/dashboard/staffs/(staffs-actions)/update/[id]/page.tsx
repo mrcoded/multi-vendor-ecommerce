5247b2c95fec562f033df020fef7c6e554ee1d3a
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-24 15:42:06 +0100

    feat: created an update/[id] page for unique staff

---
+++ src/app/dashboard/staffs/(staffs-actions)/update/[id]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import StaffForm from "@/components/forms/StaffForm";
+import FormHeader from "@/app/dashboard/_components/shared/FormHeader";
+
+const UpdateStaff = async ({ params: { id } }: { params: { id: string } }) => {
+  const staff = await getData(`staffs/${id}`);
+
+  return (
+    <div>
+      <FormHeader title="Update Staff" />
+      <StaffForm updateData={staff} />
+    </div>
+  );
+};
+
+export default UpdateStaff;
