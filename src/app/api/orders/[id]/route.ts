a8467cfdbd76ad2b54ec19f78e7dcc3459891b6b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-24 15:40:53 +0100

    feat: created a DELETE and GET api endpoints for orders/[id] request

---
+++ src/app/api/orders/[id]/route.ts
@@ -0,0 +1,85 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const order = await db.order.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        orderItems: true,
+      },
+    });
+
+    return NextResponse.json(order);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch Order",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const existingOrder = await db.order.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingOrder) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "Order Not Found",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    const deletedOrder = await db.order.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json(deletedOrder);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to Delete Order",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
