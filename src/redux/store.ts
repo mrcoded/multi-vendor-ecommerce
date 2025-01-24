018e197f65a777586c7527cbd6f4d9df2a93c9e3
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-24 15:52:02 +0100

    feat: added checkoutSllice to redux store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as cartSlice } from "./slices/cartSlice";
+import { reducer as checkoutSlice } from "./slices/checkoutSlice";
 
 //Create the Store
 export const store = configureStore({
   reducer: {
     //Slices go here
     cart: cartSlice,
+    checkout: checkoutSlice,
   },
 });
