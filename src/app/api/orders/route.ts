689a26b012f00043fe025e9ee85b8b7c4268d9d4
CodedLibra <adeola.ogundijo@yahoo.com>
2025-02-01 15:57:17 +0100

    fat: updated the POST request api for orders

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -8,6 +8,7 @@ interface OrderItemProps {
   qty: string;
   salePrice: string;
   title: string;
+  vendorId: string;
 }
 
 function generateOrderNumber(length: number) {
@@ -23,8 +24,6 @@ function generateOrderNumber(length: number) {
 }
 
 export async function POST(request: Request) {
-  const prisma = new PrismaClient();
-
   try {
     const { checkoutFormData, orderItems } = await request.json();
     const {
@@ -41,36 +40,68 @@ export async function POST(request: Request) {
       paymentMethod,
     } = checkoutFormData;
 
-    const newOrder = await db.order.create({
-      data: {
-        userId,
-        firstName,
-        lastName,
-        emailAddress,
-        phone,
-        streetAddress,
-        city,
-        country,
-        district,
-        shippingCost: parseFloat(shippingCost),
-        paymentMethod,
-        orderNumber: generateOrderNumber(8),
-      },
+    //Use the prisma transaction API
+    const result = await db.$transaction(async (prisma) => {
+      //create order and orderitems within the transaction
+
+      const newOrder = await prisma.order.create({
+        data: {
+          userId,
+          firstName,
+          lastName,
+          emailAddress,
+          phone,
+          streetAddress,
+          city,
+          country,
+          district,
+          shippingCost: parseFloat(shippingCost),
+          paymentMethod,
+          orderNumber: generateOrderNumber(8),
+        },
+      });
+
+      //Create Order Item
+      const newOrderItems = await prisma.orderItem.createMany({
+        data: orderItems.map((item: OrderItemProps) => ({
+          productId: item.id,
+          vendorId: item.vendorId,
+          quantity: parseInt(item.qty),
+          price: parseFloat(item.salePrice),
+          orderId: newOrder.id, //associate the order with the item
+          imageUrl: item.imageUrl,
+          title: item.title,
+        })),
+      });
+
+      //Calculate total amount for each product and create a sale for each
+      const sales = await Promise.all(
+        orderItems.map(async (item: OrderItemProps) => {
+          const totalAmount = parseFloat(item.salePrice) * parseInt(item.qty);
+
+          const newSale = await prisma.sale.create({
+            data: {
+              orderId: newOrder.id, //associate the order with the item
+              productId: item.id,
+              vendorId: item.vendorId,
+              productQty: parseInt(item.qty),
+              productTitle: item.title,
+              productImageUrl: item.imageUrl,
+              productPrice: parseFloat(item.salePrice),
+              total: totalAmount,
+            },
+          });
+
+          return newSale;
+        })
+      );
+
+      return { newOrder, newOrderItems, sales };
     });
 
-    //Create Order Item
-    await prisma.orderItem.createMany({
-      data: orderItems.map((item: OrderItemProps) => ({
-        productId: item.id,
-        quantity: parseInt(item.qty),
-        price: parseFloat(item.salePrice),
-        orderId: newOrder.id,
-        imageUrl: item.imageUrl,
-        title: item.title,
-      })),
-    });
+    console.log(result.newOrder, result.newOrderItems, result.sales);
 
-    return NextResponse.json(newOrder);
+    return NextResponse.json(result.newOrder);
   } catch (error) {
     console.log(error);
 
