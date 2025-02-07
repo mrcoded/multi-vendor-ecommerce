24c84a13edd61fc7ef0977ec3be3644ee7761b87
CodedLibra <adeola.ogundijo@yahoo.com>
2025-02-08 00:48:16 +0100

    feat: created a PUT api endpoint for customers/[id] route

---
+++ src/app/api/customers/[id]/route.ts
@@ -0,0 +1,80 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function PUT(
+  request: Request,
+  {
+    params: { id },
+  }: {
+    params: { id: string };
+  }
+) {
+  try {
+    const {
+      name,
+      phone,
+      email,
+      profileImage,
+      streetAddress,
+      city,
+      district,
+      firstName,
+      lastName,
+      userName,
+      dateOfBirth,
+      country,
+      userId,
+    } = await request.json();
+
+    const existingUser = await db.userProfile.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        {
+          data: null,
+          message: "User Profile not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    const updatedUser = await db.userProfile.update({
+      where: {
+       id,
+      },
+      data: {
+        name,
+        phone,
+        email,
+        profileImage,
+        streetAddress,
+        city,
+        district,
+        firstName,
+        lastName,
+        userName,
+        dateOfBirth,
+        country,
+        userId,
+      },
+    });
+
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to update User",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
