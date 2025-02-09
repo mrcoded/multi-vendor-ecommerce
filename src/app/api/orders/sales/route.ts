7666c1ee23bb45b015f6ce9e5b2aa18fecfbd765
CodedLibra <adeola.ogundijo@yahoo.com>
2025-02-09 22:53:54 +0100

    feat: created a GET api endpoint for orders/sales route

---
+++ src/app/api/orders/sales/route.ts
@@ -0,0 +1,27 @@
+import { db } from "@/lib/db";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const sales = await db.sale.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return NextResponse.json(sales);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      {
+        message: "Unable to fetch Sales",
+        error,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
