33e0dec5406237c3401983bf05d66fb63e2d43ee
CodedLibra <adeola.ogundijo@yahoo.com>
2025-04-23 00:44:44 +0100

    feat: created a search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import FilterComponent from "@/components/Filters/FilterComponent";
+
+async function page({ searchParams }: { searchParams: string }) {
+  const searchParamsObject = new URLSearchParams(searchParams);
+  const sort = searchParamsObject.get("sort");
+  const min = searchParamsObject.get("min");
+  const max = searchParamsObject.get("max");
+  const search = searchParamsObject.get("search");
+  const page = searchParamsObject.get("page") || 1;
+
+  const category = {
+    title: "",
+    slug: "",
+    products: [],
+    isSearch: true,
+  };
+
+  let products;
+
+  if (search) {
+    products = await getData(`products?search=${search}`);
+  } else {
+    products = await getData(`products?search=`);
+  }
+
+  return (
+    <div>
+      <FilterComponent category={category} products={products} />
+    </div>
+  );
+}
+
+export default page;
