41c2c5b5b551de32657f835ecae76d12ae8c0242
CodedLibra <adeola.ogundijo@yahoo.com>
2025-04-23 00:46:35 +0100

    feat: created a category/[slug] page

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import getData from "@/lib/getData";
+
+import FilterComponent from "@/components/Filters/FilterComponent";
+
+async function page({
+  params: { slug },
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: string;
+}) {
+  const searchParamsObject = new URLSearchParams(searchParams);
+  const sort = searchParamsObject.get("sort");
+  const min = searchParamsObject.get("min");
+  const max = searchParamsObject.get("max");
+  const page = searchParamsObject.get("page") || 1;
+
+  const category = await getData(`categories/filter/${slug}`);
+
+  let products;
+
+  if (page) {
+    products = await getData(`products?catId=${category.id}&page=${page}`);
+  } else if (max && min) {
+    products = await getData(
+      `products?catId=${category.id}&sort=asc&min=${min}&max=${max}`
+    );
+  } else if (min) {
+    products = await getData(
+      `products?catId=${category.id}&sort=${sort}&min=${min}`
+    );
+  } else if (max) {
+    products = await getData(
+      `products?catId=${category.id}&sort=${sort}&max=${max}`
+    );
+  } else {
+    products = await getData(`products?catId=${category.id}`);
+  }
+
+  return (
+    <div>
+      <FilterComponent category={category} products={products} />
+    </div>
+  );
+}
+
+export default page;
