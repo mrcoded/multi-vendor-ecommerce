bdb1522b33b6a14e2ae719090cd4dbeb49e84b58
CodedLibra <adeola.ogundijo@yahoo.com>
2025-04-25 23:02:57 +0100

    feat: generated functions to use in component extracted frm useSearchparams

--- src/components/Filters/BreadCrumb.tsx
+++ src/components/Filters/BreadCrumb.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
 import { ChevronRight } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 
 function BreadCrumb({
   title,
@@ -9,6 +12,12 @@ function BreadCrumb({
   title: string;
   resultCount: number;
 }) {
+  const searchParams = useSearchParams();
+  const pageSize = 3;
+  const currentPage = searchParams.get("page") || "1";
+  const startRange = (parseInt(currentPage) - 1) * pageSize + 1;
+  const endRange = Math.ceil(parseInt(currentPage) * pageSize);
+
   return (
     <div className="flex items-center justify-between text-xs">
       <div className="flex items-center">
@@ -16,7 +25,9 @@ function BreadCrumb({
         <ChevronRight className="size-5" />
         <p>{title}</p>
       </div>
-      <p>1-40 of {resultCount} results</p>
+      <p>
+        {startRange}-{endRange} of {resultCount} results
+      </p>
     </div>
   );
 }
