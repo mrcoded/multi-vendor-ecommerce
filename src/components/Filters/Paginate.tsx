5b6e0dc0a3122c99adb186fdd68e13d122a7ce13
CodedLibra <adeola.ogundijo@yahoo.com>
2025-06-30 22:41:09 +0100

    feat: create a Paginate component

---
+++ src/components/Filters/Paginate.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+
+function Paginate({ totalPages }: { totalPages: number }) {
+  const searchParams = useSearchParams();
+  const currentPage = searchParams.get("page") || "1";
+  console.log(currentPage, totalPages);
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            href={`${parseInt(currentPage) == 1 ? `?${new URLSearchParams({ page: "1" })}` : `?${new URLSearchParams({ page: (parseInt(currentPage) - 1).toString() })}`}`}
+          />
+        </PaginationItem>
+        {totalPages <= 3 ? (
+          Array.from({ length: 3 }, (_, index) => {
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  isActive={index + 1 === parseInt(currentPage)}
+                  href={`?${new URLSearchParams({ page: (index + 1).toString() })}`}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            );
+          })
+        ) : (
+          <>
+            {Array.from({ length: 3 }, (_, index) => {
+              return (
+                <PaginationItem key={index}>
+                  <PaginationLink href="#">{index + 1}</PaginationLink>
+                </PaginationItem>
+              );
+            })}
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          </>
+        )}
+        <PaginationItem>
+          <PaginationNext
+            href={`${parseInt(currentPage) == totalPages ? `?${new URLSearchParams({ page: `${totalPages}` })}` : `?${new URLSearchParams({ page: `${parseInt(currentPage) + 1}` })}`}`}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+}
+
+export default Paginate;
