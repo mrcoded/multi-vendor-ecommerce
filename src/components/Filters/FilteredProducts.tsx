1c2a76d0a273899bd9e39533d9f4263fa7ac783e
CodedLibra <adeola.ogundijo@yahoo.com>
2025-06-30 22:42:23 +0100

    feat: passed products and productCount props to Product component

--- src/components/Filters/FilteredProducts.tsx
+++ src/components/Filters/FilteredProducts.tsx
@@ -1,42 +1,26 @@
 import React from "react";
 
-import getData from "@/lib/getData";
-
 import Product from "../Product";
 import Paginate from "./Paginate";
 
-interface ProductsProp {
-  products: {
-    slug: string;
-    id: string;
-    title: string;
-    imageUrl: string;
-    salePrice: number;
-  }[];
-  productCount: number;
-}
+import { ProductsProp } from "@/types/Products";
 
-async function FilteredProducts({ products = [], productCount }: ProductsProp) {
+async function FilteredProducts({
+  products = [],
+  productCount,
+}: {
+  products: ProductsProp["products"];
+  productCount: number;
+}) {
   //PAGINATION
   const pageSize = 3;
   const totalPages = Math.ceil(productCount / pageSize);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {products.map(
-        (
-          product: {
-            slug: string;
-            id: string;
-            title: string;
-            imageUrl: string;
-            salePrice: number;
-          },
-          i: React.Key
-        ) => {
-          return <Product key={i} product={product} />;
-        }
-      )}
+      {products.map((product, i: React.Key) => {
+        return <Product key={i} product={product} />;
+      })}
       <div className="p-8 mx-auto flex items-center justify-center w-full">
         <Paginate totalPages={totalPages} />
       </div>
