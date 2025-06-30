067e335ad06ebb276c41b977cad59b167074bd6c
CodedLibra <adeola.ogundijo@yahoo.com>
2025-06-30 22:25:21 +0100

    created a priceRanges constant file

---
+++ src/app/constants/priceRanges.ts
@@ -0,0 +1,20 @@
+export const priceRanges = [
+  {
+    display: "Under 300",
+    max: 300,
+  },
+  {
+    display: "Between 300 and 500",
+    max: 500,
+    min: 300,
+  },
+  {
+    display: "Between 500 and 700",
+    max: 700,
+    min: 500,
+  },
+  {
+    display: "Above 700",
+    min: 700,
+  },
+];
