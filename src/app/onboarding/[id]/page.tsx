9e0c12be40c1add87cd65401daf6ade0cd02158b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-04 00:02:45 +0100

    refactor

--- src/app/onboarding/[id]/page.tsx
+++ src/app/onboarding/[id]/page.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 
-import getData from "@/lib/getData";
-import VendorForm from "@/components/forms/VendorForm";
+import Steps from "./_components/Steps";
+import StepForm from "./_components/StepForm";
 
-const page = async ({ params: { id } }: { params: { id: string } }) => {
-  //GET user by ID
-  const user = await getData(`users/${id}`);
+function page({ params }: { params: { id: string } }) {
+  const steps = [
+    { index: 1, title: "Personal Details" },
+    { index: 2, title: "Other Details" },
+    { index: 3, title: "Vendor Details Summary" },
+  ];
 
   return (
-    <div className="flex flex-col gap-6 p-16">
-      <div className="max-w-4xl p-4 mx-auto">
-        <h2>Hello {user?.name} Tell Us More About Yourself</h2>
+    <div className="bg-slate-200 dark:bg-slate-950 min-h-screen">
+      <div className="max-w-3xl my-6 mx-auto p-6 border border-slate-700 rounded-lg">
+        {/* STEPS */}
+        <Steps steps={steps} />
+        <div className="w-full p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+          {/* FORM */}
+          <StepForm vendorId={params.id} />
+        </div>
       </div>
-      <VendorForm user={user} />
     </div>
   );
-};
+}
 
 export default page;
