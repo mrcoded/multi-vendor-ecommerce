fd09e6d3948d3cdba27ae8bef82e5877920790f6
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-04 21:55:27 +0100

    feat: create a formatDate lib

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,15 @@
+function formatDate(date: string) {
+  // Convert the date string to a Javascript Date object
+  const dateObject = new Date(date);
+
+  // Format the date as a string in the desired format
+  const formattedDate = dateObject.toLocaleString("default", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  return formattedDate;
+}
+
+export default formatDate;
