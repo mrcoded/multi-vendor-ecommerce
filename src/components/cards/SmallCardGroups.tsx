4ba7bbb44aeeba0216c94ff83cd72629fc0c3bd1
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-04 15:49:11 +0100

    feat: created and passed datas to SmallCard component

--- src/components/cards/SmallCardGroups.tsx
+++ src/components/cards/SmallCardGroups.tsx
@@ -3,29 +3,53 @@ import { CheckCheck, Loader, RefreshCcw, ShoppingCart } from "lucide-react";
 
 import SmallCard from "./SmallCard";
 
-function SmallCardGroups() {
+function SmallCardGroups({ orders }: { orders: any }) {
+  const status = {
+    pending: "PENDING",
+    processing: "PROCESSING",
+    delivering: "DELIVERED",
+    shipping: "SHIPPING",
+    cancelling: "CANCELLED",
+  };
+
+  //function to get the count of orders
+  const getOrdersCountByStatus = (status: string) => {
+    return orders
+      .filter((order: { orderStatus: string }) => order.orderStatus === status)
+      .length.toString()
+      .padStart(2, "0");
+  };
+
+  // Get the count of orders
+  const totalOrdersCount = orders.length.toString().padStart(2, "0");
+  const pendingOrders = getOrdersCountByStatus(status.pending);
+  const shippingOrders = getOrdersCountByStatus(status.shipping);
+  const processedOrders = getOrdersCountByStatus(status.processing);
+  const deliveredOrders = getOrdersCountByStatus(status.delivering);
+  const cancellingOrders = getOrdersCountByStatus(status.cancelling);
+
   const orderStatus = [
     {
       title: "Total Orders",
-      number: 500,
+      number: totalOrdersCount,
       iconBg: "bg-green-600",
       icon: ShoppingCart,
     },
     {
       title: "Orders Pending",
-      number: 100,
+      number: pendingOrders,
       iconBg: "bg-blue-600",
       icon: Loader,
     },
     {
       title: "Order Processing",
-      number: 200,
+      number: processedOrders,
       iconBg: "bg-orange-600",
       icon: RefreshCcw,
     },
     {
       title: "Orders Delivered",
-      number: 300,
+      number: deliveredOrders,
       iconBg: "bg-purple-600",
       icon: CheckCheck,
     },
