4ae620a438e585854b25ffe9836f77ca8c4e37f5
CodedLibra <adeola.ogundijo@yahoo.com>
2025-07-04 15:46:53 +0100

    feat: created a dashboard  page

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+
+import getData from "@/lib/getData";
+
+import Heading from "@/components/shared/Heading";
+import UserDashboard from "./_components/dashboard/UserDashboard";
+import VendorDashboard from "./_components/dashboard/VendorDashboard";
+import LargeCardGroups from "@/components/cards/LargeCardGroups";
+import SmallCardGroups from "@/components/cards/SmallCardGroups";
+import CustomDataTable from "@/components/tables/CustomDataTable";
+import DashboardCharts from "./_components/charts/DashboardCharts";
+
+const page = async () => {
+  const session = await getServerSession(authOptions);
+  const role = session?.user?.role;
+
+  const orders = await getData("orders");
+  const sales = await getData("orders/sales");
+  const products = await getData("products");
+  //Set User and Vendor Dashboard
+  if (role === "USER") return <UserDashboard />;
+  if (role === "VENDOR") return <VendorDashboard />;
+
+  return (
+    <div>
+      <Heading title="Dashboard Overview" />
+      {/* Large Cards */}
+      <LargeCardGroups sales={sales} />
+      {/* Small Cards */}
+      <SmallCardGroups orders={orders} />
+      {/* Charts */}
+      <DashboardCharts sales={sales} />
+      {/* Recent Orders Table */}
+      {/* <CustomDataTable /> */}
+    </div>
+  );
+};
+
+export default page;
