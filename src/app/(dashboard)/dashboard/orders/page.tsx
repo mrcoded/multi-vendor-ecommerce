9b9c795d8775f76c3f7ac340d1e4c26c8a336668
CodedLibra <adeola.ogundijo@yahoo.com>
2026-03-18 00:04:55 +0100

    feat: rendered OrderCard component based on user role data

--- src/app/(dashboard)/dashboard/orders/page.tsx
+++ src/app/(dashboard)/dashboard/orders/page.tsx
@@ -1,51 +1,97 @@
 import React from "react";
+import { getServerSession } from "next-auth";
 
 import getData from "@/lib/getData";
-import OrderCard from "./_components/OrderCard";
-import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
+import OrderCard from "./_components/OrderCard";
+import Paginate from "@/components/Filters/Paginate";
 import { OrderCardProps } from "@/types/order";
 
-const Page = async () => {
+async function OrdersPage() {
   //Fetch all orders
-  const orders = await getData("orders");
+  const orders: OrderCardProps[] = await getData("orders");
   //Get the userId
   const session = await getServerSession(authOptions);
   if (!session) return;
 
   const userId = session?.user?.id;
+  const role = session?.user?.role;
 
-  if (orders.length === 0 || !orders) {
-    return <p>No Order Yet</p>;
-  }
   //Filter by userId
-  const userOrders = orders.filter(
+  const userOrders = orders?.filter(
     (order: { userId: string }) => order.userId === userId,
   );
 
+  //Filter by vendorId
+  const vendorOrders = orders?.filter(
+    (order: { orderItems: Array<{ vendorId: string }> }) =>
+      order.orderItems.filter((item: { vendorId: string }) => {
+        console.log(item.vendorId, userId);
+        return item.vendorId === userId;
+      }),
+  );
+  // console.log(userOrders, vendorOrders, orders);
+  //PAGINATION
+  const pageSize = 10;
+  const productCount = orders?.length;
+  const totalPages = Math.ceil(productCount / pageSize);
+
   return (
-    <section className="py-12 bg-white sm:py-16 lg:py-20">
-      <div className="px-4 m-auto sm:px-6 lg:px-8 max-w-7xl">
-        <div className="max-w-6xl mx-auto">
-          <div>
-            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+    <section className="lg:py-4 min-h-screen">
+      <div className="max-w-7xl mx-auto">
+        {/* Header */}
+        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
+          <div className="">
+            <h1 className="text-2xl font-bold dark:text-muted-foreground text-gray-900 sm:text-3xl">
               Your Orders
             </h1>
-            <p className="mt-2 text-sm font-normal text-gray-600">
+            <p className="mt-2 text-sm font-normal dark:text-muted-foreground text-gray-600">
               Check the status of recent and old orders & discover more products
             </p>
           </div>
+        </div>
 
-          <ul className="mt-8 space-y-5 lg:mt-12 sm:space-y-6 lg:space-y-10">
-            {userOrders.map((order: OrderCardProps, i: number) => {
-              return <OrderCard key={i} order={order} />;
-            })}{" "}
-          </ul>
+        {/* Orders List */}
+        <ul className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-3 mt-8 lg:mt-12 ">
+          {role === "USER" ? (
+            userOrders?.length > 0 ? (
+              userOrders.map((order, i) => <OrderCard key={i} order={order} />)
+            ) : (
+              <div className="col-span-full text-center py-20 bg-white rounded-lg border border-dashed border-gray-300">
+                <p className="text-gray-500">No orders found.</p>
+              </div>
+            )
+          ) : role === "VENDOR" ? (
+            vendorOrders?.length > 0 ? (
+              vendorOrders.map((order, i) => (
+                <OrderCard key={i} order={order} />
+              ))
+            ) : (
+              <div className="col-span-full text-center py-20 bg-white rounded-lg border border-dashed border-gray-300">
+                <p className="text-gray-500">No orders found.</p>
+              </div>
+            )
+          ) : role === "ADMIN" ? (
+            orders?.length > 0 ? (
+              orders.map((order, i) => <OrderCard key={i} order={order} />)
+            ) : (
+              <div className="col-span-full text-center py-20 bg-white rounded-lg border border-dashed border-gray-300">
+                <p className="text-gray-500">No orders found.</p>
+              </div>
+            )
+          ) : (
+            <></>
+          )}
+        </ul>
+
+        {/* Pagination Section */}
+        <div className="mt-5 xl:mt-12 flex justify-center">
+          <Paginate totalPages={totalPages} productCount={productCount} />
         </div>
       </div>
     </section>
   );
-};
+}
 
-export default Page;
+export default OrdersPage;
